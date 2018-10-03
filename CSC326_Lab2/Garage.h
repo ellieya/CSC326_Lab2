a3c1a5c2018a60c9f1407992dad38fd09c0b5de7
ellieya <j.chen.ellie@gmail.com>
2018-10-03 10:33:31 -0400

    Small update before lab starts: Finished car class, setting up garage class; need to make sure garage class working as intended. Need to clean up and figure out driver. Seems we're almost done actually

--- CSC326_Lab2/Garage.h
+++ CSC326_Lab2/Garage.h
@@ -3,6 +3,9 @@
 
 using namespace std;
 
-class garage{
+#include "ArrayStack.h"
+#include "car.h"
 
+class garage : public ArrayStack<car> {
+	int MAX_STACK = 10;
 };
--- CSC326_Lab2/car.h
+++ CSC326_Lab2/car.h
@@ -1,1 +1,21 @@
-#pragma once
+#include <string>
+
+using namespace std;
+
+class car {
+	string license;
+	int move_count;
+
+public:
+	string get_license();
+	void increase_move_count();
+};
+
+
+string car::get_license() {
+	return license;
+}
+
+void car::increase_move_count() {
+	move_count++;
+}
